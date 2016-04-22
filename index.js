da2f71a458a00801dbdb9bbfc0ad2b0de893c4a0
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-04-22 19:04:38 -0400

    Parser complete, need to refactor and compartmentalize code

--- index.js
+++ index.js
@@ -24,9 +24,13 @@ app.get('/', function (req, res) {
 
 	// console.log();
 	// var deferred = q.defer();
-	request(prepare_query('201701', 'BIOL', '301'), function (error, response, body) {
+	// console.log(typeof(req.query.subject));
+	// console.log(req.query.code);
+	var query = prepare_query('201701', req.query.subject.toUpperCase(), req.query.code);
+	// console.log(query);
+	request(query, function (error, response, body) {
 		// if ( error ) { return deferred.reject( error ); }
-		if (error) { res.send('ERROR OCCURED!'); }
+		if (error) { res.send({error:'ERROR OCCURED!'}); }
 		// deferred.resolve({body:body,response:response});
 		// console.log(body);
 		var $ = cheerio.load(body);
@@ -37,12 +41,31 @@ app.get('/', function (req, res) {
 			return $(this).children().length > 1;
 		});
 
-		$relevant_tables.map(function(index, element){
-			console.log('index is '+index)
-			console.log($(this).text());
-		})
 
-		res.send({ body:body });
+		var courses = [];
+		// map all the TRs:
+		$relevant_tables.map( function (index, element) {
+			// this loop has the TRs as elements
+
+			$(this).filter(function(i,e){
+				// here we try to remove the TRs which do not have course
+				// details in them, these are defined as those which do not
+				// have numeric CRNS in the 1st TD
+				var possible_crn = $(e).children().eq(1).text();
+				return !isNaN(+possible_crn);
+			}).each(function(i,e){
+				// e is a single TR here:
+				// get it's chldren which are the TDs:
+				var to_be_saved = {};
+				$(e).children().each(function (i_index, information) {
+					to_be_saved[information_map(i_index)] = $(information).text();
+				});
+				courses.push(to_be_saved);
+			});
+		});
+		// console.log(courses);
+
+		res.send({ 'courses':courses });
 	});
 
 })
@@ -51,6 +74,50 @@ app.listen( app.get('port'), function() {
 	console.log('running now on port ', app.get('port') );
 })
 
+function information_map(index){
+	switch (index) {
+		case 0:
+			return false;
+		case 1:
+			return 'CRN';
+		case 2:
+			return 'subject';
+		case 3:
+			return 'course_code';
+		case 4:
+			return 'section';
+		case 5:
+			return 'type';
+		case 6:
+			return 'credits';
+		case 7:
+			return 'title';
+		case 8:
+			return 'days';
+		case 9:
+			return 'time';
+		case 10:
+			return 'capacity';
+		case 11:
+			return 'enrolled';
+		case 12:
+			return 'available';
+		case 13:
+			return 'WLcapacity';
+		case 14:
+			return 'WLenrolled';
+		case 15:
+			return 'WLremain';
+		case 16:
+			return 'instructor';
+		case 17:
+			return 'date';
+		case 18:
+			return 'location';
+		case 19:
+			return 'status';
+	}
+}
 
 function prepare_query ( term, subject, code, title ) {
 	// Prepares a query url to search the schedule
