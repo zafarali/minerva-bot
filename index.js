3721fb474d9b6b6d5b7cc44f579e98152b4c86a9
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-05-15 11:23:07 -0400

    Close #1,#2,#3 update index with new working mechanisms

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 'use strict';
-
+var q = require('q');
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var app = express();
-var cheerio = require('cheerio');
-var q = require('q');
-var chat = require('./chat_utils.js');
-var search = require('./search.js');
+var fs = require('fs');
+var path = require('path');
+var chat = require('./chat_utils.js')
 
+/// import relevant plugins
+var minerva_search = require('./plugins').minerva_search;
+var help = require('./plugins').help;
+var conversation = require('./plugins').conversation;
+var showmore = require('./plugins').showmore;
+
+
+// set up web server
+var app = express();
 app.set('port', ( process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000 ) );
 app.set('ip', ( process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1') );
 
@@ -28,26 +35,22 @@ app.use( bodyParser.urlencoded( { extended:false} ) );
 app.use( bodyParser.json() );
 
 
+// context management:
+
+var contexts = JSON.parse(fs.readFileSync(path.resolve('./', 'history.json'), 'utf8')).contexts || {};
+
+function get_or_create_context(user){
+	if(!contexts[user]){
+		contexts[user] = {extracted:[]};
+	} 
+	return contexts[user];
+}
+
+
 app.get('/', function (req, res) { 
-	// result.send('Minnerva Bot!');
 
-	// console.log();
-	// var deferred = q.defer();
-	// console.log(typeof(req.query.subject));
-	// console.log(req.query.code);
-	res.send({error:'API is no longer accessible via get request'})
-	// var query = prepare_query('201701', req.query.subject, req.query.code);
-	// // console.log(query);
-	// request(query, function (error, response, body) {
-	// 	// if ( error ) { return deferred.reject( error ); }
-	// 	if (error) { res.send({error:'ERROR OCCURED!'}); }
-	// 	// deferred.resolve({body:body,response:response});
-	// 	// console.log(body);
-	// 	var courses = parse_data(body);
-	// 	// console.log(courses);
-
-	// 	res.send({ 'courses':courses });
-	// });
+		// storage.push(req.query['to_push']);
+		res.send({message:'minerva bot backend!'});
 
 });
 
@@ -60,9 +63,9 @@ app.get('/webhook', function (req, res) {
 	res.send('Error, wrong token');
 });
 
+
 app.post('/webhook/', function (req, res) {
-
-
+	// handles messages and postbacks
 
 	var messaging_events = req.body.entry[0].messaging;
 
@@ -70,42 +73,91 @@ app.post('/webhook/', function (req, res) {
 		var event = messaging_events[i];
 		var sender = event.sender.id;
 
-		if(event.message && event.message.text){
+		// handle explicit text messaging events or postbacks
+		if( (event.message && event.message.text ) 
+			|| (event.postback && event.postback.payload ) ){
 
-			var args = {
-				queries: [],
-				replies: []
+			// set a welcome message
+			chat.welcome();
+
+			var query, postback;
+
+			if(event.postback){
+				query = '' // if postback is defined, then query must be blank
+				postback = event.postback.payload;
+			}else{
+				postback = '' // message is defined, then postback must be blank
+				query = event.message.text;
 			}
-			var text = event.message.text;
-			console.log('query recieved:', text);
-			chat.parse(text, args);
-			
-			var total_queryes = args.queries.length;
 
-			var result = q(args);
-			for (var i = 0; i < args.queries.length; i++) {
-				result = result.then(search.query_execute);
-			};
+			console.log('query recieved:', query);
+			console.log('postback recieved:', postback);
 
-			result.then(function(){
-				// reply all the messages
-				if(args.replies.length === 0){
-					chat.reply(sender, "I couldn\'t find any courses for that in the fall or winter :(")
-				}else{
-					for (var i = 0; i < args.replies.length; i++) {
-						chat.reply(sender, args.replies[i]);
-					};
+			internals(query, sender, postback).then(function(ctx){
+
+				console.log('response computed.');
+
+				if(ctx.replies.length === 0){
+					ctx.replies.push('I don\'t understand what you\'re asking me :(. I\'m always improving. Until then, you can ask me to HELP you.');
 				}
 
-			}, function(err){
-				// reply the error
-				chat.reply(sender, 'I\'m sorry, something went wrong with the search');
-				console.log('Error occured:', err);
-			})
+				// schedule replies every 200ms
+				chat.reply2(sender,ctx.replies)
+
+
+			}).then(function(){
+				
+				console.log('reply chain complete');
+
+			}).catch(function(err){
+				console.log('error occured:',err)
+				chat.reply(sender, "Something went wrong... Try again!", TESTTOKEN);
+				res.send({error:err});
+			});
 		}
 	}
 
 	res.sendStatus(200);
+
+});
+
+
+// internal chain logic for the purposes of abstraction
+function internals(query, user, postback){
+
+	// plugins to be executed
+	var to_execute = [ conversation, help, showmore, minerva_search ];
+
+	// obtain a context
+	var history = get_or_create_context(user);
+	
+	context = {
+		history: history,
+		current_query: query,
+		postback:postback,
+		completed: false,
+		replies:[]
+	}
+	
+	return to_execute.reduce(q.when, q(context));
+}
+
+
+app.get('/dump_history/', function(req, res){
+	// dumps all user contexts to the file system and out
+	// only works if the token query is correct
+	// for "security" 
+	if(req.query['token'] == process.env['VERIFYTOKEN']) {
+		// dumps all user contexts to the file system and out
+		var contexts_string = JSON.stringify({contexts:contexts});
+		
+		fs.writeFile('./history.json', contexts_string, function (err) {
+			if (err) throw err;
+			// console.log('It\'s saved!');
+		});
+
+		res.send({contexts:contexts});
+	}
 });
 
 app.listen( app.get('port'), app.get('ip'), function() {
