c6122c5d1badb498d5de4243ae8b14d0c2f9c0f4
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-06-04 17:44:59 -0400

    Fix typo in conversation, add some side conversations for small talk

--- plugins/help/index.js
+++ plugins/help/index.js
@@ -12,17 +12,29 @@ function help_me(context){
 	for (var i = 0; i < tokenized_input.length; i++) {
 	
 		if(utils.arrays.contains(WORDS['helpwords'], tokenized_input[i])){
-			context.replies.push(utils.arrays.random_choice(['I got you!', 'Of course', 'Everyone needs help sometimes!']))
-			context.replies.push('I can answer questions about:\n'+
-				'Specific courses: "What time is COMP 202 in the Fall"\n'+
-				'General courses: "Courses about Quantum Mechanics" or "500 level biology courses"\n'+
-				'Buildings at McGill: "Where is Dispatch?\n'+
-				'uPrint Locations: "Are there printers in Genome Center?"');
-			context.completed = true;
+			set_user_needs_help(context);
 			break;
 		}
 	}
+
+	if( context.current_query.match(/(what|wut){1}.*(capabi|abil|do|purpo){1}/gi) ){
+		set_user_needs_help(context);
+	}
+
 	return context;
+}
+
+
+function set_user_needs_help(context){
+	context.replies.push(
+		utils.arrays.random_choice(
+			['I got you!', 'Of course', 'Everyone needs help sometimes!']));
+	context.replies.push('I can answer questions about:\n'+
+		'Specific courses: "What time is COMP 202 in the Fall"\n'+
+		'General courses: "Courses about Quantum Mechanics" or "500 level biology courses"\n'+
+		'Buildings at McGill: "Where is Dispatch?\n'+
+		'uPrint Locations: "Are there printers in Genome Center?"');
+	context.completed = true;
 }
 
 module.exports = help_me;
